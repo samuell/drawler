2f2d52042d8e4e7a8297b5695b2b4bf68e01b152
Samuel Lampa <samuel.lampa@gmail.com>
2013-01-02 00:51:36 +0100

    Tabs -> 4 spaces

--- drawler.d
+++ drawler.d
@@ -2,53 +2,53 @@ import std.net.curl, std.stdio, std.regex, std.algorithm, std.string;
 import arsd.mysql;
 
 void main(string[] args) {
-	if (args.length < 2) {
-		writeln("Error: You must specify an URL, like so: ./drawler http://example.com");
-	} else {
-		string currentUrl = args[1]; // Get the URL to start with, from the command line 
-		string htmlData = cast(string) get(currentUrl); // Read the webpage (cast from char[] to string)
-		string[] urls = extractUrls(htmlData, currentUrl);
-		
-		foreach(l; urls) {
-			writefln("Found url: %s", l);
-		}
+    if (args.length < 2) {
+        writeln("Error: You must specify an URL, like so: ./drawler http://example.com");
+    } else {
+        string currentUrl = args[1]; // Get the URL to start with, from the command line 
+        string htmlData = cast(string) get(currentUrl); // Read the webpage (cast from char[] to string)
+        string[] urls = extractUrls(htmlData, currentUrl);
+        
+        foreach(l; urls) {
+            writefln("Found url: %s", l);
+        }
 
-		/*
-		auto mysql = new MySql("localhost", "drawler", "drawler", "drawler");
-	
-		// ? based placeholders do conversion and escaping for you
-		foreach(line; mysql.query("select siteid, linkid from links where siteid > ?", 5)) {
-	         // access columns by name
-	         writefln("%s: %s", line["siteid"], line["linkid"]);
-	    }
-		*/
-	}
+        /*
+        auto mysql = new MySql("localhost", "drawler", "drawler", "drawler");
+    
+        // ? based placeholders do conversion and escaping for you
+        foreach(line; mysql.query("select siteid, linkid from links where siteid > ?", 5)) {
+             // access columns by name
+             writefln("%s: %s", line["siteid"], line["linkid"]);
+        }
+        */
+    }
 }
 
 string[] extractUrls(string htmlData, string currentUrl) {
-	string[] urls;
-	// Create compiled RegEx (for fast execution) 
-	enum url_regex = ctRegex!("href=\"([^\"]+)\"","g");
+    string[] urls;
+    // Create compiled RegEx (for fast execution) 
+    enum url_regex = ctRegex!("href=\"([^\"]+)\"","g");
 
-	foreach(match; match(htmlData, url_regex)) {
-		string url = match.captures[1];
-		url = stripTrailingSlash(url);
-		url = ensureAbsoluteUrl(url, currentUrl);
-		urls ~= url; // Add url to the urls array
-	}
-	return urls; 
+    foreach(match; match(htmlData, url_regex)) {
+        string url = match.captures[1];
+        url = stripTrailingSlash(url);
+        url = ensureAbsoluteUrl(url, currentUrl);
+        urls ~= url; // Add url to the urls array
+    }
+    return urls; 
 }
 
 string stripTrailingSlash(string url) {
-	if (url.endsWith("/")) {
-		url = chop(url); 
-	}
-	return url;
+    if (url.endsWith("/")) {
+        url = chop(url); 
+    }
+    return url;
 }
 
 string ensureAbsoluteUrl(string url, string currentUrl) {
-	if (url.startsWith("/")) {
-		url = currentUrl ~ url;
-	}
-	return url;
-}	
+    if (url.startsWith("/")) {
+        url = currentUrl ~ url;
+    }
+    return url;
+}    
