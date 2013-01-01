5fd188a4985145824f357d43868de02603d721de
Samuel Lampa <samuel.lampa@gmail.com>
2013-01-01 23:53:07 +0100

    Adding dbschema definition

---
+++ dbschema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `links` (
+  `linkid` int(11) NOT NULL auto_increment,
+  `siteid` int(11) default NULL,
+  `url` varchar(255) NOT NULL,
+  `title` varchar(200) default NULL,
+  `description` varchar(255) default NULL,
+  `fulltext` mediumtext,
+  `indexdate` date default NULL,
+  `level` int(11) default NULL,
+  PRIMARY KEY  (`linkid`),
+  KEY `url` (`url`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
