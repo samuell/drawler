9dc5c98e05fd622f3b35456e4f441aea71a653be
Samuel Lampa <samuel.lampa@gmail.com>
2013-01-02 11:26:28 +0100

    Added content of extractTitle method

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ setup_sql:
 	mysql -u drawler --password=drawler drawler < dbschema.sql
 
 build:
-	dmd drawler.d arsd/mysql.d arsd/database.d -L-lphobos2 -L-lcurl -version=MySQL_51
+	dmd drawler.d arsd/mysql.d arsd/database.d -L-lphobos2 -L-lcurl -version=MySQL_51 -O
 
 run: build
 	./drawler http://saml.rilspace.org
--- drawler.d
+++ drawler.d
@@ -5,18 +5,18 @@ void main(string[] args) {
     if (args.length < 2) {
         writeln("Error: You must specify an URL, like so: ./drawler http://example.com");
     } else {
-        string currentUrl = args[1]; // Get the URL to start with, from the command line 
+        string currentUrl = args[1]; // Get the URL to start with, from the command line
         string htmlData = cast(string) get(currentUrl); // Read the webpage (cast from char[] to string)
 
         string[] urls = extractUrls(htmlData, currentUrl);
         urls = urls ~ currentUrl;
-        
+
         string title = extractTitle(htmlData);
         string fulltext = extractFulltext(htmlData);
-        
+
         writefln("Title: %s", title); // TODO: Remove debug code
-        // writefln("Fulltext: \n%s", fulltext); // TODO: Remove debug code
-        
+        //writefln("Fulltext: \n%s", fulltext); // TODO: Remove debug code
+
         MySqlConnection mySqlConn = new MySqlConnection;
         mySqlConn.addLinks(urls);
         mySqlConn.updateUrlWithFulltext(currentUrl, fulltext);
@@ -25,7 +25,7 @@ void main(string[] args) {
 
 string[] extractUrls(string htmlData, string currentUrl) {
     string[] urls;
-    // Create compiled RegEx (for fast execution) 
+    // Create compiled RegEx (for fast execution)
     enum urlRegex = ctRegex!("href=\"([^\"]+)\"","g");
 
     foreach(match; match(htmlData, urlRegex)) {
@@ -34,21 +34,23 @@ string[] extractUrls(string htmlData, string currentUrl) {
         url = ensureAbsoluteUrl(url, currentUrl);
         urls ~= url; // Add url to the urls array
     }
-    return urls; 
+    return urls;
 }
 
 string extractTitle(string htmlData) {
-    string title;
-    return title; 
+    enum titleRegex = ctRegex!(r"<title.*?>(.*?)</title>","g");
+    auto match = match(htmlData, titleRegex );
+    string title = match.captures[1];
+    return title;
 }
 
 string extractFulltext(string htmlData) {
     string fullText;
     string bodyHtml;
-    
+
     // Remove newline characters
     htmlData = join(htmlData.splitLines(), " ");
-     
+
     enum bodyRegex = ctRegex!(r"<body.*?>(.*?)</body>","g");
     auto match = match(htmlData, bodyRegex);
     bodyHtml = match.captures[1];
@@ -60,7 +62,7 @@ string extractFulltext(string htmlData) {
     // Remove excess whitespace / tabs
     enum excessSpaceRegex = ctRegex!(r"[\s\t]+","g");
     fullText = fullText.replace(excessSpaceRegex," ");
-    
+
     // Remove non-word characters
     enum unallowedCharsRegex = ctRegex!(r"[^A-Za-z0-9\-\.\,\sÅÄÖåäö]","g");
     fullText = fullText.replace(unallowedCharsRegex,"");
@@ -70,7 +72,7 @@ string extractFulltext(string htmlData) {
 
 string stripTrailingSlash(string url) {
     if (url.endsWith("/")) {
-        url = chop(url); 
+        url = chop(url);
     }
     return url;
 }
@@ -80,7 +82,7 @@ string ensureAbsoluteUrl(string url, string currentUrl) {
         url = currentUrl ~ url;
     }
     return url;
-}    
+}
 
 class MySqlConnection {
     MySql mysql;
@@ -95,16 +97,16 @@ class MySqlConnection {
         foreach(line; this.mysql.query("SELECT siteid, linkid FROM links")) {
              // access columns by name
              writefln("%s: %s", line["siteid"], line["linkid"]);
-        } 
+        }
         return links;
     }
-    
+
     void addLinks(string[] links) {
         foreach(link; links) {
             this.mysql.query("INSERT INTO drawler.links ( url ) VALUES ( ? );", link);
         }
     }
-    
+
     void updateUrlWithFulltext(string url, string fulltext) {
         this.mysql.query("UPDATE links SET fulltxt='" ~ fulltext ~ "' WHERE url='" ~ url ~ "';");
     }
